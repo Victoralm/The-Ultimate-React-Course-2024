8d2ee6a2f79c97f849e3b3596ce01c33c1c6b604
Victoralm <Victoralm@users.noreply.github.com>
2024-05-22 17:46:13 -0300

    S05L36 - Creating and Reusing Components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { pizzaData } from "./data";
 
 function App() {
-    return <h1>Hello React...</h1>;
+    return (
+        <div>
+            <h1>Hello React...</h1>
+            <Pizza /> {/*Nesting the Pizza component into the App component*/}
+            <Pizza />
+            <Pizza />
+        </div>
+    );
+}
+
+// The new Pizza component
+function Pizza() {
+    // return <h2>Pizza</h2>;
+    return (
+        <div>
+            <img src={pizzaData[0].photoName} alt={pizzaData[0].name}/>
+            <h2>{pizzaData[0].name}</h2>
+            <p>{pizzaData[0].ingredients}</p>
+        </div>
+    );
 }
 
 // React 18
